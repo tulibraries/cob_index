2f57e559ac359a02c5e644d43604a139d92f7343
David Kinzer <dtkinzer@gmail.com>
2019-09-19 10:05:29 -0400

    Add basic "CobIndex::CLI.ingest spec"

--- spec/cob_index_spec.rb
+++ spec/cob_index_spec.rb
@@ -4,4 +4,22 @@ RSpec.describe CobIndex do
   it "has a version number" do
     expect(CobIndex::VERSION).not_to be nil
   end
+
+  describe "ingest" do
+    before(:example) do
+      @indexer = instance_double("Traject::Indexer::MarcIndexer")
+
+      allow(Traject::Indexer::MarcIndexer).to receive(:new).and_return(@indexer)
+      allow(@indexer).to receive_messages(load_config_file: nil, process: nil)
+    end
+
+    after(:example) do
+      CobIndex::CLI.ingest
+    end
+
+    it "loads indexer" do
+      expect(@indexer).to receive(:load_config_file)
+      expect(@indexer).to receive(:process)
+    end
+  end
 end

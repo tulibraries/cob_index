d7ed0f1a908c0749384829062ef54c895362d293
David Kinzer <dtkinzer@gmail.com>
2020-06-11 15:36:27 -0400

    Update test name.
    Remove unused single quote.

--- spec/cob_index/macros/custom_spec.rb
+++ spec/cob_index/macros/custom_spec.rb
@@ -48,41 +48,39 @@ RSpec.describe "custom methods" do
     end
 
     it "extracts the first possible 4 digit numeral" do
-      expect(four_digit_year "'18-1966.,1918   ").to eq("1966")
+      expect(four_digit_year "18-1966.,1918   ").to eq("1966")
       expect(four_digit_year "c1993.,1993").to eq("1993")
       expect(four_digit_year "©2012,2012").to eq("2012")
     end
   end
 
-  describe "#to_marc_normalized" do
-    describe "#flank(field)" do
-      let(:input) {}
-      subject { Traject::Macros::Custom.flank input }
-      context "nil" do
-        it "returns an empty string" do
-          expect(subject).to be_nil
-        end
+  describe "#flank(field)" do
+    let(:input) {}
+    subject { Traject::Macros::Custom.flank input }
+    context "nil" do
+      it "returns an empty string" do
+        expect(subject).to be_nil
       end
+    end
 
-      context "empty string" do
-        let(:input) { "" }
-        it "returns an empty string" do
-          expect(subject).to eq("")
-        end
+    context "empty string" do
+      let(:input) { "" }
+      it "returns an empty string" do
+        expect(subject).to eq("")
       end
+    end
 
-      context "non empty string" do
-        let(:input) { "foo" }
-        it "returns a flanked string" do
-          expect(subject).to eq("matchbeginswith foo matchendswith")
-        end
+    context "non empty string" do
+      let(:input) { "foo" }
+      it "returns a flanked string" do
+        expect(subject).to eq("matchbeginswith foo matchendswith")
       end
+    end
 
-      context "a string that is flanked" do
-        let(:input) { "matchbeginswith foo matchendswith" }
-        it "does not reflank a string" do
-          expect(subject).to eq(input)
-        end
+    context "a string that is flanked" do
+      let(:input) { "matchbeginswith foo matchendswith" }
+      it "does not reflank a string" do
+        expect(subject).to eq(input)
       end
     end
   end

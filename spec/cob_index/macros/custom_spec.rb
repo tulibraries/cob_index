9b61667ae89b4da6f589a2bf9cfe38a30dfaaad2
David Kinzer <dtkinzer@gmail.com>
2020-06-11 15:25:08 -0400

    Fix syntax.

--- spec/cob_index/macros/custom_spec.rb
+++ spec/cob_index/macros/custom_spec.rb
@@ -13,46 +13,44 @@ include Traject::Macros::Custom
 
 RSpec.describe "custom methods" do
 
-  describe "#four_digit_year(field):" do
-    describe "#four_digit_year(field)" do
-      context "when field is nil" do
-        it "returns nil" do
-          expect(four_digit_year nil).to eq(nil)
-        end
+  describe "#four_digit_year(field)" do
+    context "when field is nil" do
+      it "returns nil" do
+        expect(four_digit_year nil).to eq(nil)
       end
+    end
 
-      context "when given an empty string" do
-        it "returns nil" do
-          expect(four_digit_year "").to eq(nil)
-          expect(four_digit_year "\n").to eq(nil)
-          expect(four_digit_year "\n\n").to eq(nil)
-          expect(four_digit_year "      ").to eq(nil)
-        end
+    context "when given an empty string" do
+      it "returns nil" do
+        expect(four_digit_year "").to eq(nil)
+        expect(four_digit_year "\n").to eq(nil)
+        expect(four_digit_year "\n\n").to eq(nil)
+        expect(four_digit_year "      ").to eq(nil)
       end
+    end
 
-      context "when contains Roman Numerals" do
-        it "returns nil" do
-          expect(four_digit_year "MCCXLV").to eq(nil)
-        end
+    context "when contains Roman Numerals" do
+      it "returns nil" do
+        expect(four_digit_year "MCCXLV").to eq(nil)
       end
+    end
 
-      it "returns nil for [n.d.],''" do
-        expect(four_digit_year '[n.d.],""').to eq(nil)
-      end
+    it "returns nil for [n.d.],''" do
+      expect(four_digit_year '[n.d.],""').to eq(nil)
+    end
 
-      it "extracts year from MCCXLV [1745],1745" do
-        expect(four_digit_year "MCCXLV [1745],1745").to eq("1745")
-      end
+    it "extracts year from MCCXLV [1745],1745" do
+      expect(four_digit_year "MCCXLV [1745],1745").to eq("1745")
+    end
 
-      it "extracts the first possible 4 digit numeral" do
-        expect(four_digit_year "1918-1966.,1918   ").to eq("1918")
-      end
+    it "extracts the first possible 4 digit numeral" do
+      expect(four_digit_year "1918-1966.,1918   ").to eq("1918")
+    end
 
-      it "extracts the first possible 4 digit numeral" do
-        expect(four_digit_year "'18-1966.,1918   ").to eq("1966")
-        expect(four_digit_year "c1993.,1993").to eq("1993")
-        expect(four_digit_year "©2012,2012").to eq("2012")
-      end
+    it "extracts the first possible 4 digit numeral" do
+      expect(four_digit_year "'18-1966.,1918   ").to eq("1966")
+      expect(four_digit_year "c1993.,1993").to eq("1993")
+      expect(four_digit_year "©2012,2012").to eq("2012")
     end
   end
 
